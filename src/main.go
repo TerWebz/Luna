470504f55721ccbe3b11092134c59c4c4a853760
Javier164 <64364181+Javier164@users.noreply.github.com>
2020-12-17 09:26:44 -0500

    Update main.go

--- src/main.go
+++ src/main.go
@@ -9,12 +9,17 @@ import (
 
 func download() (err error) {
 	var name string
-	fmt.Printf("Enter a game name: ")
-	fmt.Scanf("%s", &name)	
+	var base string	
+	var game string
+	var uri string
+	
+	base = "https://javier06.ejemplo.me/"
 
-	var base string = "https://javier06.ejemplo.me/"
-	var game string = name + ".ciso"
-	var uri string = base + game
+	fmt.Printf("Enter a game name: ")
+	fmt.Scanf("%s", &name)
+
+	game = name + ".ciso"
+	uri = base + game
 
 	response, err := http.Get(uri)
 	if err != nil {
@@ -23,20 +28,17 @@ func download() (err error) {
 	}
 
 	if response.StatusCode != 200 {
-   		fmt.Println("Error: The servers are down or the URL/Game is invalid.")
+   		fmt.Println("Error: The servers are down or the URL/Game that has been entered is invalid.")
 		os.Exit(1)
     	} else {
-		fmt.Println("Downloading...")
-	}
-
-   	defer response.Body.Close()
+		fmt.Printf("Downloading %s... \n", game)
+	} // This is before anything because we want to determine if the user put a valid game before we make the file, to not make garbage or filler files.
+    	defer response.Body.Close()
 
 	file, err := os.Create(game)
-
 	if err != nil {
 		return err
 	}
-
 	defer file.Close()
 
 	_, err = io.Copy(file, response.Body)
@@ -48,7 +50,7 @@ func download() (err error) {
 }
 
 func main() {
-	var title string = "Luna v1.0.1"
+	var title string = "Luna v1.0.1.1"
 	fmt.Println(title)
 	download();
 } 
